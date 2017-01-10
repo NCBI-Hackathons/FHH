4001c2935047b58e6e2053263b5f161f6e27445f
informationista <lisa.federer@nih.gov>
2017-01-10 14:09:12 -0500

    Initial commit
    Create the JS file and the list of variables that calculate the score

---
+++ risk/lung_cancer/lung.js
@@ -0,0 +1,18 @@
+var score = {
+	'age':0,
+	'family_cancer':0,
+	'copd':0,
+	'personal_cancer':0,
+	'bmi':0,
+	'white':0,
+	'black':0,
+	'hispanic':0,
+	'pac_islander':0,
+	'native_amer':0,
+	'current_smoker':0,
+	'daily_cigarettes':0,
+	'years_quit':0,
+	'years_smoked':0,
+	'education':0,
+	'total':0
+};
